3e6be9d5a25049504001f030bd0b43f1fcb7b3e1
Yao-Salomon <salomon.yao@consultech.ci>
2023-10-08 11:09:56 +0000

    init setups

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import {NextUIProvider} from "@nextui-org/react";
+
+export default function Home() {
+  return (
+    <NextUIProvider>
+      <div>
+        
+      </div>
+    </NextUIProvider>
+    
+  )
+}
